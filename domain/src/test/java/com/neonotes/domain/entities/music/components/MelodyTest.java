d4c56462bc7ce0455f88529b319054230d3d178d
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-07 16:29:01 -0300

    test: transpose methods of melody

--- domain/src/test/java/com/neonotes/domain/entities/music/components/MelodyTest.java
+++ domain/src/test/java/com/neonotes/domain/entities/music/components/MelodyTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MelodyTest {
 
     private static Melody melody;
+
     @BeforeAll
     static void setUp(){
         melody = new Melody();
@@ -20,9 +21,36 @@ public class MelodyTest {
     }
 
     @Test
-    @DisplayName("Should return correct transposition of note sequence")
+    @DisplayName("Should return correct minimum transposition of note sequence")
     void shouldReturnCorrectMinimumSequenceTranspose(){
         MusicComponent transposedMelody = melody.shiftTone(1);
+        MusicComponent expectedMelody = new Melody(List.of(Note.G_SHARP,Note.A_SHARP,Note.C));
+
+        assertEquals(expectedMelody,transposedMelody);
+    }
+
+    @Test
+    @DisplayName("Should return correct maximum transposition of note sequence")
+    void shouldReturnCorrectMaximumSequenceTranspose(){
+        MusicComponent transposedMelody = melody.shiftTone(11);
+        MusicComponent expectedMelody = new Melody(List.of(Note.F_SHARP,Note.G_SHARP,Note.A_SHARP));
+
+        assertEquals(expectedMelody,transposedMelody);
+    }
+
+    @Test
+    @DisplayName("Should return correct minimum negative transposition of note sequence")
+    void shouldReturnCorrectMinimumNegativeSequenceTranspose(){
+        MusicComponent transposedMelody = melody.shiftTone(-1);
+        MusicComponent expectedMelody = new Melody(List.of(Note.F_SHARP,Note.G_SHARP,Note.A_SHARP));
+
+        assertEquals(expectedMelody,transposedMelody);
+    }
+
+    @Test
+    @DisplayName("Should return correct maximum negative transposition of note sequence")
+    void shouldReturnCorrectMaximumNegativeSequenceTranspose(){
+        MusicComponent transposedMelody = melody.shiftTone(-11);
         MusicComponent expectedMelody = new Melody(List.of(Note.G_SHARP,Note.A_SHARP,Note.C));
 
         assertEquals(expectedMelody,transposedMelody);
