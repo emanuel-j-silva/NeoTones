8d8f3862d8b54b3908bb7ddc4385f4ee2966fd1d
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-08 11:20:47 -0300

    feat: implements find user use case

--- domain/src/main/java/com/neonotes/domain/usecases/user/FindUserUseCase.java
+++ domain/src/main/java/com/neonotes/domain/usecases/user/FindUserUseCase.java
@@ -1,4 +1,36 @@
 package com.neonotes.domain.usecases.user;
 
+import com.neonotes.domain.usecases.exceptions.EntityNotFoundException;
+import com.neonotes.domain.usecases.user.dto.UserDTO;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
 public class FindUserUseCase {
+
+    private final UserRepository userRepository;
+
+    public FindUserUseCase(UserRepository userRepository) {
+        this.userRepository = Objects.requireNonNull(userRepository,"User repository can't be null");
+    }
+
+    public UserDTO findByUuid(UUID uuid){
+        return userRepository.findByUuid(uuid)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
+
+    public UserDTO findByUsername(String username){
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
+
+    public UserDTO findByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
+
+    public List<UserDTO> findAll(){
+        return  userRepository.findAll();
+    }
 }
