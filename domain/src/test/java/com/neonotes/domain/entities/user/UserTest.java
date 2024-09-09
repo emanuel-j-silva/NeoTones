c2fd4e06e793730f6575af5c4faa38b4c033e720
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-09 13:54:10 -0300

    test: implementing user entity tests

---
+++ domain/src/test/java/com/neonotes/domain/entities/user/UserTest.java
@@ -0,0 +1,51 @@
+package com.neonotes.domain.entities.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    @Test
+    @DisplayName("Should create user without informing uuid")
+    void createUserWithoutUuid(){
+        User user = new User("test","email","password");
+
+        assertNotNull(user.getUuid());
+        assertEquals("test",user.getUsername());
+        assertEquals("email",user.getEmail());
+        assertEquals("password",user.getPassword());
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trying to create user with null username")
+    void createUserWithNullUsername(){
+        assertThrows(NullPointerException.class, ()-> new User(null,"email","password"));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trying to create user with null email")
+    void createUserWithNullEmail(){
+        assertThrows(NullPointerException.class, ()-> new User("test",null,"password"));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trying to create user with null password")
+    void createUserWithNullPassword(){
+        assertThrows(NullPointerException.class, ()-> new User("test","email",null));
+    }
+
+    @Test
+    @DisplayName("Should users with same username and UUID be the same")
+    void shouldUsersBeTheSame(){
+        UUID uuid = UUID.randomUUID();
+        User user1 = new User(uuid,"test","email","password");
+        User user2 = new User(uuid,"test","anotherEmail","anotherPassword");
+
+        assertEquals(user1,user2);
+        assertEquals(user1.hashCode(),user2.hashCode());
+    }
+}
