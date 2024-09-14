1830de7a78b1e570038b554978cc8cecda2a6728
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-14 09:21:26 -0300

    feat: add some methods to tone class

--- domain/src/main/java/com/neotones/domain/entities/music/note/Tone.java
+++ domain/src/main/java/com/neotones/domain/entities/music/note/Tone.java
@@ -1,12 +1,14 @@
 package com.neotones.domain.entities.music.note;
 
+import java.util.Objects;
+
 public class Tone {
     private final Note note;
-    private final ScaleType scale;
+    private final ScaleType scaleType;
 
-    public Tone(Note note, ScaleType scale) {
-        this.note = note;
-        this.scale = scale;
+    public Tone(Note note, ScaleType scaleType) {
+        this.note = Objects.requireNonNull(note,"Note of tone can't be null");
+        this.scaleType = Objects.requireNonNull(scaleType, "Scale type of tone can't be null");
     }
 
     public Note getNote() {
@@ -14,7 +16,26 @@ public class Tone {
     }
 
     public ScaleType getScale() {
-        return scale;
+        return scaleType;
     }
 
+    @Override
+    public String toString() {
+        return "Tone{" +
+                "note=" + note +
+                ", scale=" + scaleType +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tone tone)) return false;
+        return note == tone.note && scaleType == tone.scaleType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(note, scaleType);
+    }
 }
