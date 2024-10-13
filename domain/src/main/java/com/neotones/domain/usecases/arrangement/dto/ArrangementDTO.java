21f3a8abfb5401aa2aa145d4a339703b3e0e221e
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-13 18:49:43 -0300

    fix: fixing attribute of arrangement dto

--- domain/src/main/java/com/neotones/domain/usecases/arrangement/dto/ArrangementDTO.java
+++ domain/src/main/java/com/neotones/domain/usecases/arrangement/dto/ArrangementDTO.java
@@ -1,6 +1,8 @@
 package com.neotones.domain.usecases.arrangement.dto;
 
+import com.neotones.domain.entities.music.components.MusicComponent;
+
 import java.util.List;
 
-public record ArrangementDTO(ToneDTO tone, List<MusicComponentDTO> components) {
+public record ArrangementDTO(ToneDTO tone, List<MusicComponent> components) {
 }
--- domain/src/main/java/com/neotones/domain/usecases/arrangement/dto/MusicComponentDTO.java
+++
@@ -1,4 +0,0 @@
-package com.neotones.domain.usecases.arrangement.dto;
-
-public record MusicComponentDTO() {
-}
