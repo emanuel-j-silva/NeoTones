2b6be35e29f0b408a89c87720dd05ead736b4f61
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-27 19:56:12 -0300

    feat: add some logic to register user use case

--- domain/src/main/java/com/neotones/domain/usecases/user/RegisterUserUseCase.java
+++ domain/src/main/java/com/neotones/domain/usecases/user/RegisterUserUseCase.java
@@ -1,6 +1,33 @@
 package com.neotones.domain.usecases.user;
 
+import com.neotones.domain.usecases.exceptions.EntityAlreadyExistsException;
+import com.neotones.domain.usecases.user.dto.RegisterUserDTO;
+import com.neotones.domain.usecases.user.dto.UserDTO;
+
+import java.util.Objects;
+import java.util.UUID;
+
 public class RegisterUserUseCase {
+    private final UserRepository userRepository;
 
+    public RegisterUserUseCase(UserRepository userRepository) {
+        this.userRepository = Objects.requireNonNull(userRepository,"user repository can't be null");
+    }
 
+    public void register(RegisterUserDTO user){
+        String errors = "";
+
+        if (userRepository.findByUsername(user.email()).isPresent()){
+            errors += "This e-mail is already in use" + "\n";
+        }
+        if (userRepository.findByUsername(user.username()).isPresent()){
+            errors +="This username is already in use";
+        }
+        if (!errors.isEmpty()){
+            throw new EntityAlreadyExistsException(errors);
+        }
+
+        UserDTO userDTO = new UserDTO(UUID.randomUUID(),user.username(), user.email(), user.password());
+        userRepository.save(userDTO);
+    }
 }
